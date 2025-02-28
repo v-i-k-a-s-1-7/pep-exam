3f2b5ca308f00dcc969762bab05292423159f852
Vikas Rajak <77117633+v-i-k-a-s-1-7@users.noreply.github.com>
2025-02-28 23:02:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")  
 def home():  
-    return "Hello, DevOps!"  
+    return "Hello, Vikas!"  
 
 if __name__ == "__main__":  
     app.run(host="0.0.0.0", port=5000)  
